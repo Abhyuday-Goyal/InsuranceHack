9ea2d9a76435825e76dec25ac24ab62d491b14f9
Abhyuday Goyal <abhyuday2106@gmail.com>
2024-04-20 06:44:47 -0400

    frontend start

---
+++ my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import InsuranceComparisons from './InsuranceComparisons';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Insurance App</h1>
+      </header>
+      <main>
+        <InsuranceComparisons />
+      </main>
+    </div>
+  );
+}
+
+export default App;
