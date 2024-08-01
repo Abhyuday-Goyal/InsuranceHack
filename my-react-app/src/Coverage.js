949fa398513408ed9a838b4e2439ceb1edd9c2fc
Ayush Roy <aroy9711@umd.edu>
2024-08-01 17:33:54 -0400

    Cosmetic Changes 2

--- my-react-app/src/Coverage.js
+++ my-react-app/src/Coverage.js
@@ -12,7 +12,7 @@ const Coverage = () => {
     const formData = new FormData();
     formData.append('pdf1', file);
     try {
-      // Send the POST request to your custom API
+      // Send the post request to your custom API
       const response = await fetch('http://127.0.0.1:5000/chat-search-data', {
         method: 'POST',
         body: formData,
